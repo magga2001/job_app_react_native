07b0a29d6e1ec12a55880d50348270d4378070f5
Switt Wattanalekhawong <switt.wattanalekhawong@kcl.ac.uk>
2023-09-06 18:51:27 +0700

    Front end of welcome component in homepage

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,31 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+//Only import useFonts for custom font
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+//When the app screen is initially loading, this will make the splash screen visible
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  return <Stack />;
+  // For custom font
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  // Loading fonts onto the screen
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      // We only want to show Homepage if the font has been loaded
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return <Stack onLayout={onLayoutRootView} />;
 };
 
 export default Layout;
